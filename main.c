cce3452142eee54e8587d9b7cceb8d0923542926
Firstein <alienor.brabant@gmail.com>
2016-03-17 11:36:59 +0100

    fix memory leak

--- main.c
+++ main.c
@@ -163,7 +163,10 @@ game init_game(int level, char* file){
     pieces[i] = new_piece(x, y, width, height, move_x, move_y);
   }
   fclose(f);
-  return new_game(g_width, g_height, nb_pieces, pieces);
+  game g = new_game(g_width, g_height, nb_pieces, pieces);
+  for(int i=0; i<=nb_pieces; ++i)
+    delete_piece(pieces[i]);
+  return g;
 }
 
 
