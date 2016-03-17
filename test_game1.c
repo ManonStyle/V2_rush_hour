6e12b98b1d08a14ac5b1978fc9fcf81b02a70337
Manon Philippot <manon.philippot@gmail.com>
2016-03-17 11:56:13 +0100

    correction test_game1.c

--- test_game1.c
+++ test_game1.c
@@ -27,7 +27,7 @@ bool test_equality_bool(bool expected, bool value, char * msg) {
   return expected == value;
 }
 
-piece pieces[NB_PIECES];
+piece pieces[NB_PIECES+1];
 /* configue de test rh
 ......
 ....2.
@@ -43,14 +43,15 @@ void set_up_rh() {
   pieces[2] = new_piece_rh(4, 2, false, false);
   pieces[3] = new_piece_rh(4, 1, true, true);
   pieces[4] = new_piece_rh(2, 1, true, false);
+  pieces[5] = new_piece_rh(4, 3, true, true);
 }
 
 void tear_down_rh() {
-  for (int i = 0 ; i < NB_PIECES; i++)
+  for (int i = 0 ; i < NB_PIECES+1; i++)
     delete_piece(pieces[i]);
 }
 
-piece piecesBis[NB_PIECES];
+piece piecesBis[NB_PIECES+1];
 /* configue de test Bis 
 4.....
 ...00.
@@ -65,23 +66,23 @@ void set_up_Bis() {
   piecesBis[2] = new_piece(0, 0, 1, 1, true, false);
   piecesBis[3] = new_piece(5, 0, 1, 1, false, true);
   piecesBis[4] = new_piece(0, 4, 1, 1, true, true);
-
+  piecesBis[5] = new_piece(2, 0, 2, 2, true, true);
 }
 
 void tear_down_Bis() {
-  for (int i = 0 ; i < NB_PIECES; i++)
+  for (int i = 0 ; i < NB_PIECES+1; i++)
     delete_piece(piecesBis[i]);
 }
 
 bool test_new_game_rh() {
   bool result = true;
   set_up_rh();
-  game g = new_game_hr(NB_PIECES-1, pieces);
+  game g = new_game_hr(NB_PIECES, pieces);
   result = result && test_equality_int(6, game_width(g), "new_game_rh game_width");
   result = result && test_equality_int(6, game_height(g), "new_game_rh game_height");
-  result = result && test_equality_int(NB_PIECES -1, game_nb_pieces(g), "new_game_rh game_nb_pieces");
+  result = result && test_equality_int(NB_PIECES, game_nb_pieces(g), "new_game_rh game_nb_pieces");
   result = result && test_equality_int(0, game_nb_moves(g), "new_game_rh game_nb_moves");
-  for (int i = 0; i < NB_PIECES-1; i++){
+  for (int i = 0; i < NB_PIECES; i++){
     result = result && test_equality_int(get_x(pieces[i]), get_x(game_piece(g, i)), "new_game_rh get_x");
     result = result && test_equality_int(get_y(pieces[i]), get_y(game_piece(g, i)), "new_game_rh get_y");
     result = result && test_equality_int(get_height(pieces[i]), get_height(game_piece(g, i)), "new_game_rh get_height");
@@ -90,7 +91,7 @@ bool test_new_game_rh() {
     result = result && test_equality_bool(can_move_y(pieces[i]), can_move_y(game_piece(g, i)), "new_game_rh can_move_y");
     }
   tear_down_rh();
-  delete_game(g);
+  //delete_game(g);
   return result; 
   }
 
@@ -215,3 +216,7 @@ int main (int argc, char *argv[]){
   else
     return EXIT_FAILURE;
  }
+
+
+//NB_PIECES+1 dans l'initialisation des tableaux pour inserer la piece fantome
+//Ajout de la pièce fantome
